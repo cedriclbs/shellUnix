15cffaca0ccbf3b37196e6e908e1c0fe56a8e075
LES BIENS cedric <cedric.les-biens@etu.u-paris.fr>
2024-11-27 15:54:30 +0100

    erreur compilation résolu + git ignore remis

--- src/builtins.c
+++ src/builtins.c
@@ -13,7 +13,7 @@ int execute_builtin(char **args, int val) {
     if (strcmp(args[0], "cd") == 0) {
         return cmd_cd(args);
     } else if (strcmp(args[0], "exit") == 0) {
-            cmd_exit(args,val);        
+        return cmd_exit(args,val);   
     } else if (strcmp(args[0], "pwd") == 0) {
         return cmd_pwd();
     } else if (strcmp(args[0], "ftype") == 0) {
