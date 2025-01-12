b98e0c13c30b0e91b30659502d30ed5757523ce5
LOTHSAVAN hugo <lothsavh@informatique.univ-paris-diderot.fr>
2025-01-12 19:58:54 +0100

    java doc builtins

--- src/builtins.c
+++ src/builtins.c
@@ -1,3 +1,9 @@
+/**
+ * @file execute_builtin.c
+ * @brief Fichier contenant les fonctions principales pour l'exécution des commandes internes ou externes 
+ * avec gestion des redirections et des pipelines.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,10 +17,11 @@
 
 /**
  * Vérifie la présence d'une sous-chaîne dans les arguments de la commande.
+ * 
  * @param args Tableau de chaînes de caractères contenant les arguments de la commande.
- * @param argc Nombre d'éléments dans le tableau args.
+ * @param argc Nombre d'éléments dans le tableau `args`.
  * @param s Sous-chaîne à rechercher dans les arguments.
- * @return 1 si la sous-chaîne s est trouvée dans les arguments, sinon 0.
+ * @return 1 si la sous-chaîne est trouvée dans les arguments, sinon 0.
  */
 int isIn(char **args, int argc, const char *s) {
     for (int i = 0; i < argc; i++) {
@@ -27,6 +34,7 @@ int isIn(char **args, int argc, const char *s) {
 
 /**
  * Gère les redirections des entrées et sorties pour les commandes.
+ *
  * @param args Arguments de la commande.
  * @param cmd_args Référence vers le tableau d'arguments modifiés après le traitement des redirections.
  * @param input_file Référence vers la chaîne qui sera utilisée pour l'entrée redirigée.
@@ -47,10 +55,11 @@ int handle_redirections(char **args, char ***cmd_args, char **input_file, char *
 
 /**
  * Restaure les descripteurs de fichiers standard après une redirection.
+ *
  * @param stdin_copy Copie du descripteur de fichier d'entrée standard.
  * @param stdout_copy Copie du descripteur de fichier de sortie standard.
  * @param stderr_copy Copie du descripteur de fichier d'erreurs standard.
- * @return 0 toujours, indiquant que la restauration a été effectuée.
+ * @return Toujours 0, indiquant que la restauration a été effectuée.
  */
 int restore_descriptors(int stdin_copy, int stdout_copy, int stderr_copy) {
     dup2(stdin_copy, STDIN_FILENO);
@@ -63,31 +72,32 @@ int restore_descriptors(int stdin_copy, int stdout_copy, int stderr_copy) {
 }
 
 /**
- * Exécute une commande en gérant les erreurs de syntaxe et de redirection.
+ * Exécute une commande interne ou externe.
+ *
  * @param cmd_args Tableau des arguments de la commande.
  * @param cmd_size Taille du tableau d'arguments.
- * @param val Valeur à utiliser pour certaines commandes internes (ex. 'exit' ou 'for').
+ * @param val Valeur à utiliser pour certaines commandes internes (exemple : `exit` ou `for`).
  * @return Le code de retour de la commande exécutée.
  */
 int execute_command(char **cmd_args, int cmd_size, int val) {
     int result = 0;
     if (strcmp(cmd_args[0], "cd") == 0) {
         if (cmd_size > 2) {
-            perror("cd: il y a trop d'argument");
+            perror("cd: trop d'arguments");
             result = 1;
         } else {
             result = cmd_cd(cmd_args);
         }
     } else if (strcmp(cmd_args[0], "exit") == 0) {
         if (cmd_size > 2) {
-            perror("exit: il y a trop d'argument");
+            perror("exit: trop d'arguments");
             result = 1;
         } else {
             result = cmd_exit(cmd_args, val);
         }
     } else if (strcmp(cmd_args[0], "pwd") == 0) {
         if (cmd_size > 1) {
-            perror("pwd: il y a trop d'argument");
+            perror("pwd: trop d'arguments");
             result = 1;
         } else {
             result = cmd_pwd();
@@ -100,18 +110,25 @@ int execute_command(char **cmd_args, int cmd_size, int val) {
     return result;
 }
 
-int isThereDelimiterOutside(char **args, char *delimiter){
-    int in_braces=0;
-    int isDelimiterOutside=0;
-    int i=0;
+/**
+ * Vérifie si un délimiteur se trouve en dehors des accolades dans les arguments.
+ *
+ * @param args Tableau de chaînes de caractères contenant les arguments.
+ * @param delimiter Délimiteur à rechercher (exemple : `|` ou `;`).
+ * @return 1 si le délimiteur est trouvé en dehors des accolades, sinon 0.
+ */
+int isThereDelimiterOutside(char **args, char *delimiter) {
+    int in_braces = 0;
+    int isDelimiterOutside = 0;
+    int i = 0;
 
     while (args[i] != NULL) {
         if (strcmp(args[i], "{") == 0) {
-            in_braces++;  // Entrée dans une paire d'accolades
+            in_braces++; // Entrée dans une paire d'accolades
         } else if (strcmp(args[i], "}") == 0) {
-            in_braces--;  // Sortie de la paire d'accolades
-        } else if (in_braces == 0 && strcmp(args[i], delimiter) == 0) {  
-            isDelimiterOutside =1;
+            in_braces--; // Sortie de la paire d'accolades
+        } else if (in_braces == 0 && strcmp(args[i], delimiter) == 0) {
+            isDelimiterOutside = 1;
         }
         i++;
     }
@@ -121,9 +138,10 @@ int isThereDelimiterOutside(char **args, char *delimiter){
 
 /**
  * Exécute une commande interne ou externe avec gestion des redirections.
+ *
  * @param args Tableau de chaînes de caractères représentant les arguments de la commande.
  * @param argc Nombre d'arguments dans le tableau `args`.
- * @param val Valeur passée à certaines commandes internes (ex. 'for' ou 'exit').
+ * @param val Valeur passée à certaines commandes internes (exemple : `for` ou `exit`).
  * @return Un entier représentant le code de retour de la commande exécutée.
  */
 int execute_builtin(char **args, int argc, int val) {
@@ -131,21 +149,22 @@ int execute_builtin(char **args, int argc, int val) {
         return val;
     }
 
-    if(isThereDelimiterOutside(args,"|") == 1){
+    if (isThereDelimiterOutside(args, "|") == 1) {
         return cmd_pipelines(args);
-    } else if(isThereDelimiterOutside(args,";") == 1){
+    } else if (isThereDelimiterOutside(args, ";") == 1) {
         return cmd_line(args);
-    } else if(strcmp(args[0],"for") == 0){
-        return cmd_for(args,argc,val);
-    } else if(strcmp(args[0],"if") == 0){
-        return cmd_if(args,val);
+    } else if (strcmp(args[0], "for") == 0) {
+        return cmd_for(args, argc, val);
+    } else if (strcmp(args[0], "if") == 0) {
+        return cmd_if(args, val);
     } else {
         char *input_file = NULL, *output_file = NULL, *error_file = NULL;
         int output_flags = 0, error_flags = 0;
         size_t cmd_size = argc;
         char **cmd_args;
 
-        if (handle_redirections(args, &cmd_args, &input_file, &output_file, &error_file, &output_flags, &error_flags, &cmd_size) != 0) {        free(cmd_args);
+        if (handle_redirections(args, &cmd_args, &input_file, &output_file, &error_file, &output_flags, &error_flags, &cmd_size) != 0) {
+            free(cmd_args);
             return 1;
         }
 
@@ -154,7 +173,7 @@ int execute_builtin(char **args, int argc, int val) {
         int saved_stderr = dup(STDERR_FILENO);
 
         if (saved_stdin == -1 || saved_stdout == -1 || saved_stderr == -1) {
-            perror("dup failed");
+            perror("dup échoué");
             free(cmd_args);
             return 1;
         }
@@ -167,7 +186,7 @@ int execute_builtin(char **args, int argc, int val) {
 
         int result = execute_command(cmd_args, cmd_size, val);
         restore_descriptors(saved_stdin, saved_stdout, saved_stderr);
-        
+
         free(cmd_args);
         return result;
     }
